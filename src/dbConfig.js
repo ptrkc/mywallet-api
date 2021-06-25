c9ffa1ec3662940ed6032f4569e366b09d2373da
ptrkc <40273827+ptrkc@users.noreply.github.com>
2021-06-25 02:37:27 -0300

    Changes to connect heroku db

--- src/dbConfig.js
+++ src/dbConfig.js
@@ -3,13 +3,11 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const dbConfig = {
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    database: process.env.DB_DATABASE,
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false,
+    },
 };
-
 const db = new pg.Pool(dbConfig);
 
 export default db;
