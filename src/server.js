b0225ebc15c1265045e5dcde7967b13557893afd
ptrkc <40273827+ptrkc@users.noreply.github.com>
2021-06-25 02:51:49 -0300

    Change to use heroku given port

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ import app from "./app.js";
 import dotenv from "dotenv";
 dotenv.config();
 
-const SERVER_PORT = process.env.SERVER_PORT || 4000;
+const SERVER_PORT = process.env.PORT || 4000;
 
 app.listen(SERVER_PORT, () => {
     console.log(`Server started on port ${SERVER_PORT}.`);
